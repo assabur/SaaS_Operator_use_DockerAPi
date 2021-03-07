36d7d94766f9aec863573b74e3746cb33a4dab14
assabur <maigaabdalahmohamed@gmail.com>
2021-03-07 21:17:10 +0100

    impl docker login

---
+++ depop.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+from pip._vendor.distlib.compat import raw_input
+import getpass
+import sys
+import docker
+def log() :
+    print("\tVeuillez renseigner les deux arguments de l SaaS_Operator_use_DockerAPi comme suit")
+    print("\tdepop [lien github] [tag]")
+
+if len (sys.argv)==2 and sys.argv[1]!= "help" :
+    log()
+if len (sys.argv)==2 and sys.argv[1]== "help" or len (sys.argv)==1  :
+    log()
+if len (sys.argv) ==3 :
+    mon_fichier = open("Dockerfile", "w")
+    lien = sys.argv[1]
+    mon_fichier.write(
+                    "\nFROM ubuntu:latest"
+                    "\nMAINTAINER abdalahmaiga <abdullahwin99@gmail.com>\n"
+                    "\nENV TZ=Europe/Paris\n"
+                    "\nRUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone\n"
+                    "\nRUN apt-get update\n"
+                    "\nRUN apt-get -y upgrade\n"
+                    "\nRUN apt-get install -y git\n"
+                    "\nRUN apt-get -y install apache2\n"
+                    "\nRUN mkdir /var/www/html/medibed\n"                           
+                    "\nRUN git clone "+lien +" /var/www/html/medibed\n"
+                    "\nENV APACHE_RUN_USER www-data\n"
+                    "\nENV APACHE_RUN_GROUP www-data\n"
+                    "\nENV APACHE_LOG_DIR /var/log/apache2\n"
+                    "\nENV APACHE_LOCK_DIR /var/lock/apache2\n"
+                    "\nENV APACHE_PID_FILE /var/run/apache2.pid\n"
+                    "\nEXPOSE 80\n"
+                    "\nCMD /usr/sbin/apache2ctl -D FOREGROUND")
+    mon_fichier.close()
+    """user = sys.argv[2]
+    passw = sys.argv[3]"""
+    name = sys.argv[2]
+    client = docker.from_env()
+    print("Start Building your docker image...")
+    client.images.build(path = "./",tag = name)
+    print("verification de la creation de "+name)
+    #display all the create images
+    """for x in range(len(client.images.list())):
+        print (client.images.list()[x])"""
+    print(client.images.get(name))
+    print("deployment done !!!")
+    val = input("Voulez vous effectuer un PUSH sur Docker-HUB ? Y/N  ")
+    if val =="y" or "Y":
+        login = raw_input("Votre identifiant Docker Hub: ")
+        mdp= getpass.getpass("votre mot de passe: ")
+        print(mdp)
+        print("start pushing your docker image to docker hub")
+        client.login(username= login, password= mdp)
+        for line in client.images.push(name, stream=True, decode=True):
+            print(line)
+else :
+    if len(sys.argv) >3 :
+        print (" Erreur faites depop help")
+
+#https://github.com/assabur/PHP-MySQL-CRUD-Web-Application
---
+++ sys.py
@@ -0,0 +1,12 @@
+import os
+import sys
+#understand how can i retrieve the differents argument pass on comman line us sys.argv
+print("This is the name of the program:", sys.argv[0])
+print("Argument List:", str(sys.argv))
+print("This is the name of the program:"+sys.argv[1])
+print("This is the name of the program:"+sys.argv[2])
+#fin the location of my docker installation path
+for d in sys.path:
+    if os.path.exists(os.path.join(d, "docker")):
+        print("docker is at %r" % d)
+
